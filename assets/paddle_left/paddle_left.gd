25271269505e44b0d85d7c886c3997d24e7a7a18
Charlie Cryer <ctriggerfish@gmail.com>
2025-01-25 19:04:17 +0100

    Flipper magic

---
+++ assets/paddle_left/paddle_left.gd
@@ -0,0 +1,20 @@
+class_name flipper_left
+extends CharacterBody2D
+
+@export var max_angle := -PI/4
+@export var min_angle := PI/4
+@export var speed := 40
+
+func _physics_process(delta: float) -> void:
+
+	if Input.is_action_pressed("ui_left"):
+		rotation -= (speed * delta)
+	else:
+		rotation += (speed * delta)
+		
+	if rotation < max_angle:
+		rotation = max_angle
+
+	if rotation > min_angle:
+		rotation = min_angle
+		
